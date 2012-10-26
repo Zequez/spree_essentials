43ea3bc3a058ed84900c8c53cd1703264edfd7d1
ZequeZ <zequez@gmail.com>
2012-10-26 10:30:32 -0700

    Added support of Amazon S3

--- app/models/spree/upload.rb
+++ app/models/spree/upload.rb
@@ -5,13 +5,24 @@ class Spree::Upload < ::Spree::Asset
   default_scope where(:type => "Upload") if table_exists?
   
   validate :no_attachement_errors
-  
+
+  URL_PATH = "spree/uploads/:id/:style/:basename.:extension"
+
   has_attached_file :attachment,
     :styles        => Proc.new{ |clip| clip.instance.attachment_sizes },
     :default_style => :medium,
-    :url           => "/spree/uploads/:id/:style/:basename.:extension",
-    :path          => ":rails_root/public/spree/uploads/:id/:style/:basename.:extension"
-  
+    :path => ":rails_root/public/#{URL_PATH}",
+    :url => "/#{URL_PATH}"
+
+  if Spree::Config[:use_s3]
+    attachment       = Spree::Upload.attachment_definitions[:attachment]
+    spree_attachment = Spree::Image.attachment_definitions[:attachment]
+
+    attachment.reverse_merge! spree_attachment
+    attachment[:path] = URL_PATH
+    attachment[:url]  = spree_attachment[:url]
+  end
+
   def image_content?
     attachment_content_type.match(/\/(jpeg|png|gif|tiff|x-photoshop)/)
   end
